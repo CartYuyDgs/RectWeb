cc7e4ebbf097145c54f25fd3e262a718243d4ce8
yuy <yy_dgs@126.com>
2020-12-20 14:25:25 +0800

    优化

--- AI-post/client/main.go
+++ AI-post/client/main.go
@@ -9,24 +9,23 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"time"
 )
 
 type date struct {
-	token string
-	msg string
-	photobase64 []string
+	Token       string   `json:"token"`
+	Msg         string   `json:"msg"`
+	Photobase64 []string `json:"data"`
 }
 
 func main() {
 
 	//获取参数
-	var url = flag.String("url", "http:127.0.0.1:8000", "input your url")
-	var token = flag.String("token", "bonc", "input your token")
-	var msg = flag.String("msg", "jpg", "input your photo formate")
-	var photo = flag.String("photo", "./abc.jpg", "input your photo address")
+	var url = flag.String("url", "http://172.16.96.57:12345/meter", "input your url")
+	var token = flag.String("token", "bonccvlab", "input your token")
+	var msg = flag.String("msg", "1.jpg", "input your photo formate")
+	var photo = flag.String("photo", "./abc.png", "input your photo address")
 	flag.Parse()
-	fmt.Println(*token,*msg,*photo)
+	fmt.Println(*token, *msg, *photo)
 
 	Objfile, err := os.Open(*photo)
 	defer Objfile.Close()
@@ -40,15 +39,14 @@ func main() {
 	n, _ := Objfile.Read(buffer)
 	encodestring := base64.StdEncoding.EncodeToString(buffer[:n])
 
-	fmt.Println(encodestring)
+	//fmt.Println(encodestring)
 
-	var info  = date{
-		msg: *msg,
-		token: *token,
+	var info = date{
+		Msg:   *msg,
+		Token: *token,
 	}
 
-	info.photobase64 = append(info.photobase64,encodestring)
-
+	info.Photobase64 = append(info.Photobase64, encodestring)
 
 	//post请求
 	contentType := "application/json"
@@ -59,14 +57,22 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("result: ",res)
+	fmt.Println("result: ", res)
 	return
 
 }
 
-func postSend(url string, info date, contentType string) (string, error)  {
-	client := &http.Client{Timeout: 5 * time.Second}
-	jsonStr, _ := json.Marshal(info)
+func postSend(url string, info date, contentType string) (string, error) {
+	client := &http.Client{}
+	//fmt.Println(info)
+	jsonStr, err := json.Marshal(info)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	//fmt.Printf("%v",jsonStr)
+	//fmt.Println(len(jsonStr))
+	//fmt.Println(bytes.NewBuffer(jsonStr))
 	resp, err := client.Post(url, contentType, bytes.NewBuffer(jsonStr))
 	if err != nil {
 		fmt.Println(err)
@@ -79,5 +85,5 @@ func postSend(url string, info date, contentType string) (string, error)  {
 	}
 
 	fmt.Println(resp.StatusCode)
-	return string(result),err
+	return string(result), err
 }
